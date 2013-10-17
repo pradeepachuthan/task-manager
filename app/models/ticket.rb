3e6e0a280434d9e5151577daab1bd93aedd7a921
alexanders <autoportal@email.ua>
2013-10-17 13:10:47 +0300

    added email valiadtion

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,11 +4,10 @@ class Ticket < ActiveRecord::Base
   attr_accessible :name, :email, :message, :status_id, :updated_at
 
   validates :name, presence: true,
-      length: { minimum: 5 }
+            length: {minimum: 5}
 
-  validates :email, presence: true,
-      length: { minimum: 5 }
+  validates_format_of :email, :with => /(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Z‌​a-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}/i
 
   validates :message, presence: true,
-      length: { minimum: 5 }
+            length: {minimum: 5}
 end
